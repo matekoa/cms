d9b6ef0605c2f0c71ea07179f41a6ea7f7439ba3
Matekoa Motsoasele <matekoa@gmail.com>
2022-09-17 01:35:10 +0200

    link staff stylesheet to page

---
+++ public/staff/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" media="all" href="../stylesheets/style.css">
+    <title>Staff Area</title>
+</head>
+<body>
+
+    <header>
+        <h1>IMI Staff Area</h1>
+    </header>
+
+    <navigation>
+        <ul>
+            <li><a href="index.php">Menu</a></li>
+        </ul>
+    </navigation>
+    <div id="content"></div>
+    <footer>
+        &copy; <?php echo date('Y'); ?> Indie Meets Internet
+    </footer>
+
+    
+</body>
+</html>
